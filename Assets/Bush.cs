b5dcb834768ba041e4252471764cca6fbec1c360
MT250 <40330325+MT250@users.noreply.github.com>
2021-05-11 02:58:02 +0300

    Initial commit

---
+++ Assets/Bush.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    [SerializeField] float foliageCutTime;
+
+    private float foliage;
+    private bool isBeingCutted;
+
+    private void Start()
+    {
+        foliage = foliageCutTime;
+    }
+    #region Collision detections region
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isBeingCutted = true;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (foliage > 0) foliage -= Time.deltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isBeingCutted = false;
+    }
+    #endregion
+    private void Update()
+    {
+        if (foliage <= foliageCutTime && !isBeingCutted)
+            foliage += Time.deltaTime / 3;
+    }
+
+}
---
+++ Assets/Saw.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float spinningSpeed;
+    [Space(10)]
+    [SerializeField] bool isSpinning;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+
+        if (isSpinning) transform.Rotate(new Vector3(0, spinningSpeed, 0));
+    }
+
+    private void ReadInput()
+    {
+        //Mouse input
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            isSpinning = true;
+            MoveToClickPosition();
+        }
+        else isSpinning = false;
+    }
+
+    private void MoveToClickPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit rayHit))
+        {
+            var hitPosition = rayHit.point;
+            var destination = new Vector3(hitPosition.x, transform.position.y, hitPosition.z);
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
+    }
+}
